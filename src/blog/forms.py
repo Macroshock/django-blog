7cc87da1c445640d9ae6c0c33a2cbecafa6897f4
Macroshock <macroshock.games@gmail.com>
2020-03-16 16:48:30 -0300

    Added title validation to the blog post form

--- src/blog/forms.py
+++ src/blog/forms.py
@@ -1,6 +1,16 @@
 from django import forms
 
+from .models import BlogPost
+
 class BlogPostForm(forms.Form):
   title = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Title'}))
   #slug = forms.SlugField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Slug'}))
   content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Content'}))
+
+  def clean_title(self, *args, **kwargs):
+    title = self.cleaned_data.get('title')
+    qs = BlogPost.objects.filter(title=title)
+
+    if qs.exists():
+      raise forms.ValidationError('This title has already been used.')
+    
