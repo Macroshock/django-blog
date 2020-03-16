6f7735a9fa2dc40506c236a5eb60112db9b5216e
Macroshock <macroshock.games@gmail.com>
2020-03-16 00:10:26 -0300

    Fixed form's style in order to match Bootstrap

--- src/Site_Blog/forms.py
+++ src/Site_Blog/forms.py
@@ -2,6 +2,6 @@ from django import forms
 
 
 class ContactForm(forms.Form):
-  full_name = forms.CharField()
-  email = forms.EmailField()
-  content = forms.CharField(widget=forms.Textarea)
+  full_name = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Your name'}))
+  email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Your email'}))
+  message = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Your message'}))
