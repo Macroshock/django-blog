c89e1ee454e6264d45834f504a37a92f0df60ae2
Macroshock <macroshock.games@gmail.com>
2020-03-17 22:50:01 -0300

    Added Navbar and changed the way Blog Posts are displayed

--- src/blog/models.py
+++ src/blog/models.py
@@ -12,6 +12,15 @@ class BlogPost(models.Model):
   slug = models.SlugField(unique=True)
   content = models.TextField(null=True, blank=True)
 
+  def get_absolute_url(self):
+    return f'/blog/{self.slug}'
+
+  def get_edit_url(self):
+    return f'/blog/{self.slug}/edit'
+
+  def get_delete_url(self):
+    return f'/blog/{self.slug}/delete'
+  
   @property
   def short_content(self):
     return self.content if len(self.content) < 100 else (self.content[:97] + '...')
