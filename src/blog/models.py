dcb1bc7a07a55c6eba9f211a6d15bef6f085562b
Macroshock <macroshock.games@gmail.com>
2020-03-18 23:47:27 -0300

    Added publishing date and timestamp to the blog posts

--- src/blog/models.py
+++ src/blog/models.py
@@ -11,7 +11,13 @@ class BlogPost(models.Model):
   title = models.TextField()
   slug = models.SlugField(unique=True)
   content = models.TextField(null=True, blank=True)
+  publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+  timestamp = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
 
+  class Meta:
+    ordering = ['-publish_date', '-updated', '-timestamp']
+    
   def get_absolute_url(self):
     return f'/blog/{self.slug}'
 
